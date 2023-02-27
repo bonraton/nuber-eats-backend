af1c693e7a7b4cd97e55d1d3e54cbe2fc1f39809
bonraton <bonraton@yandex.ru>
2023-02-27 17:11:40 +0100

    feat: find orsers by owner

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,10 +1,11 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { Order } from './entities/order.entity';
 import { OrdersService } from './orders.service';
 import { User } from 'src/user/entities/user.entity';
 import { Role } from 'src/auth/role.decorator';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 
 @Resolver(() => Order)
 export class OrderResolver {
@@ -17,5 +18,15 @@ export class OrderResolver {
     @Args('input') createOrderInput: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
     return this.ordersService.createOrder(customer, createOrderInput);
+  }
+
+  @Query(() => GetOrdersOutput)
+  @Role('Any')
+  async getOrders(
+    @AuthUser() user: User,
+    @Args('input') getOrdersInput: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    console.log(user);
+    return this.ordersService.getOrders(user, getOrdersInput);
   }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Dish } from 'src/dish/entities/dish.entity';
 import { Restaurant } from 'src/restaurant/entities/restaurant.entity';
-import { User } from 'src/user/entities/user.entity';
+import { User, UserRole } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 
@@ -90,6 +91,51 @@ export class OrdersService {
       return {
         ok: false,
         error: 'Could not create an order',
+      };
+    }
+  }
+
+  async getOrders(
+    user: User,
+    { status }: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    let orders: Order[];
+    try {
+      if (user.role === UserRole.Client) {
+        orders = await this.orders.find({
+          where: {
+            customer: {
+              id: user.id,
+            },
+          },
+        });
+      } else if (user.role === UserRole.Delivery) {
+        orders = await this.orders.find({
+          where: {
+            customer: {
+              id: user.id,
+            },
+          },
+        });
+      } else if (user.role === UserRole.Owner) {
+        const restaurants = await this.restaurants.find({
+          where: {
+            owner: {
+              id: user.id,
+            },
+          },
+          relations: ['orders'],
+        });
+        orders = restaurants.map((restaurant) => restaurant.orders).flat(1);
+      }
+      return {
+        ok: true,
+        orders,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not find orders',
       };
     }
   }
